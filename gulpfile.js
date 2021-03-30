a4e088c88d6cd311aaa521f194cae01c8317113d
liyichao <1120805364@qq.com>
2021-03-30 17:57:50 +0800

    简化配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,11 @@
-const {src, dest, series} = require('gulp');
+const {src, dest} = require('gulp');
 const gts = require('gulp-typescript');
-const rename = require('gulp-rename');
-const del = require('del');
 
-function scripts() {
+exports.default = () => {
   return src(['src/components/**/*.tsx', 'src/components/**/*.ts'])
     .pipe(gts.createProject('tsconfig.json', {
       declaration: true,
       emitDeclarationOnly: true
     })())
-    .pipe(dest('lib'));
-}
-
-function renameEntryScript() {
-  return src('lib/vue-virtual-tree.common.js')
-    .pipe(rename("index.js"))
-    .pipe(dest('lib'));
-}
-
-function clean() {
-  return del(['lib/vue-virtual-tree.common.js']);
-}
-
-exports.default = series(scripts, renameEntryScript, clean);
+    .pipe(dest('typings'));
+};
