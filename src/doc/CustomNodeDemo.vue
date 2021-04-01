5afa96c90ca441df518bf9ae989ef2b54e723964
liyichao <1120805364@qq.com>
2021-04-01 15:20:05 +0800

    doc ok

--- src/doc/CustomNodeDemo.vue
+++ src/doc/CustomNodeDemo.vue
@@ -6,7 +6,7 @@
 
 <script lang="tsx">
   import {defineComponent, onMounted, ref} from 'vue';
-  import {TreeInstance, TreeNodeOptions} from "../components/VirtualTree/types";
+  import {TreeNodeOptions} from "../components/VirtualTree/types";
 
   function recursion(path = '0', level = 3): TreeNodeOptions[] {
     const list = [];
